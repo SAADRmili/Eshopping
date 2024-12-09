4f2d597e2d9959233b6ac5b0f6e4d4a4f1cad085
Rmili <saadrmili45@gmail.com>
2024-12-09 13:52:33 +0100

    Finish Order Infrastructure project

---
+++ Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using Ordering.Infrastructure.Data;
+
+namespace Ordering.Infrastructure.Repositories;
+
+public class OrderRepository(OrderContext orderContext) : RespositoryBase<Order>(orderContext), IOrderRepository
+{
+    public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+    {
+        var orders = await orderContext
+            .Orders
+            .Where(x => x.UserName == userName)
+            .ToListAsync();
+        return orders;
+    }
+}
--- Services/Ordering/Ordering.Infrastructure/Repositories/RespositoryBase.cs
+++ Services/Ordering/Ordering.Infrastructure/Repositories/RespositoryBase.cs
@@ -1,38 +1,53 @@
-﻿using Ordering.Core.Common;
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Common;
 using Ordering.Core.Repositories;
+using Ordering.Infrastructure.Data;
 using System.Linq.Expressions;
 
 namespace Ordering.Infrastructure.Repositories;
-public class RespositoryBase<T> : IAsyncRepository<T> where T : EntityBase
+public class RespositoryBase<T>(OrderContext orderContext) : IAsyncRepository<T> where T : EntityBase
 {
 
-    public Task<IReadOnlyList<T>> GetAllAsync()
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+    => await orderContext
+        .Set<T>()
+        .ToListAsync();
+
+    public async Task<IReadOnlyList<T>> GetAllAsync(Expression<Func<T, bool>> predicate)
+    => await orderContext
+        .Set<T>()
+        .Where(predicate)
+        .ToListAsync();
+
+    public async Task<T> GetByIdAsync(int id)
+    => await orderContext
+        .Set<T>()
+        .FirstOrDefaultAsync(x => x.Id == id);
+
+    public async Task<T> AddAsync(T entity)
     {
-        throw new NotImplementedException();
+        orderContext
+            .Set<T>()
+            .Add(entity);
+        await orderContext.SaveChangesAsync();
+        return entity;
     }
 
-    public Task<IReadOnlyList<T>> GetAllAsync(Expression<Func<T, bool>> predicate)
+    public async Task DeleteAsync(T entity)
     {
-        throw new NotImplementedException();
+        var entitydeleted = await GetByIdAsync(entity.Id);
+        if (entitydeleted is null) return;
+        orderContext
+            .Set<T>()
+            .Remove(entitydeleted);
+        await orderContext.SaveChangesAsync();
     }
 
-    public Task<T> GetByIdAsync(int id)
+    public async Task UpdateAsync(T entity)
     {
-        throw new NotImplementedException();
-    }
-
-    public Task<T> AddAsync(T entity)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task DeleteAsync(T entity)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task UpdateAsync(T entity)
-    {
-        throw new NotImplementedException();
+        var entityUpdated = await GetByIdAsync(entity.Id);
+        if (entityUpdated is null) return;
+        orderContext.Entry(entity).State = EntityState.Modified;
+        await orderContext.SaveChangesAsync();
     }
 }
